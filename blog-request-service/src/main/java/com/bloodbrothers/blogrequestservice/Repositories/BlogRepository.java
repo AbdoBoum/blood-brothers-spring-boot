9f94eefc33083b3d8d50e6f5090c5dafd682cd81
AbdoBoum <abderrahim.boumahdi@um5s.net.ma>
2019-05-19 02:44:23 +0000

    adding all methods for blog-request-service

--- blog-request-service/src/main/java/com/bloodbrothers/blogrequestservice/Repositories/BlogRepository.java
+++ blog-request-service/src/main/java/com/bloodbrothers/blogrequestservice/Repositories/BlogRepository.java
@@ -4,7 +4,10 @@ import com.bloodbrothers.blogrequestservice.Entities.Blog;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+import java.util.List;
+
 
 @RepositoryRestResource
 public interface BlogRepository extends JpaRepository<Blog, Long> {
+    List<Blog> findBlogByTitreContains(String titre);
 }
--- blog-request-service/src/main/java/com/bloodbrothers/blogrequestservice/Repositories/RequestRepository.java
+++ blog-request-service/src/main/java/com/bloodbrothers/blogrequestservice/Repositories/RequestRepository.java
@@ -4,6 +4,10 @@ import com.bloodbrothers.blogrequestservice.Entities.Demande;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+import java.util.List;
+
 @RepositoryRestResource
 public interface RequestRepository extends JpaRepository<Demande, Long> {
+    List<Demande> findDemandeByIdCentre(long id);
+    List<Demande> findDemandeByiIAndIdGroupSang(long id);
 }
