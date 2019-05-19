6503b7d2eb692e2ec4de816b2394fc6555294869
AbdoBoum <abderrahim.boumahdi@um5s.net.ma>
2019-05-19 04:14:02 +0000

    adding authentication service

--- blog-request-service/src/main/java/com/bloodbrothers/blogrequestservice/Repositories/RequestRepository.java
+++ blog-request-service/src/main/java/com/bloodbrothers/blogrequestservice/Repositories/RequestRepository.java
@@ -2,12 +2,17 @@ package com.bloodbrothers.blogrequestservice.Repositories;
 
 import com.bloodbrothers.blogrequestservice.Entities.Demande;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 import java.util.List;
 
 @RepositoryRestResource
 public interface RequestRepository extends JpaRepository<Demande, Long> {
+
+    @Query("Select d from Demande d where d.idCentre = :id")
     List<Demande> findDemandeByIdCentre(long id);
+
+    @Query("Select d from Demande d where d.idGroupSang = :id")
     List<Demande> findDemandeByiIAndIdGroupSang(long id);
 }
