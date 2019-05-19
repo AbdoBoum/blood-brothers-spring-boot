562b4f4c48abea0ec1aa2f71fdf8b96155cc4d81
AbdoBoum <abderrahim.boumahdi@um5s.net.ma>
2019-05-19 13:45:53 +0000

    adding stock repository

--- blog-request-service/src/main/java/com/bloodbrothers/blogrequestservice/Repositories/BlogRepository.java
+++ blog-request-service/src/main/java/com/bloodbrothers/blogrequestservice/Repositories/BlogRepository.java
@@ -3,6 +3,7 @@ package com.bloodbrothers.blogrequestservice.Repositories;
 import com.bloodbrothers.blogrequestservice.Entities.Blog;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 import java.util.List;
@@ -12,5 +13,11 @@ import java.util.List;
 public interface BlogRepository extends JpaRepository<Blog, Long> {
 
     @Query("select b from Blog b where b.titre like %:msg%")
-    List<Blog> findBlogByTitreContains(String msg);
+    List<Blog> findBlogByTitreContains(@Param("msg") String msg);
+
+    //public static final String FIND_BLOG = "SELECT id_blog, titre_blog, contenu_blog, image_blog, id_donnateur FROM Blog";
+
+    //@Query(value = FIND_BLOG, nativeQuery = true)
+    //public List<Blog> findAllBlogs();
+
 }
--- donor-centre-request/src/main/java/com/blooddonation/donorcentrerequest/Repositories/DonorRepository.java
+++ donor-centre-request/src/main/java/com/blooddonation/donorcentrerequest/Repositories/DonorRepository.java
@@ -4,6 +4,18 @@ import com.blooddonation.donorcentrerequest.Entities.Donnateur;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+import java.util.Optional;
+
 @RepositoryRestResource
 public interface DonorRepository extends JpaRepository<Donnateur, Long> {
+
+    @Override
+    Optional<Donnateur> findById(Long aLong);
+
+    Optional<Donnateur> findByNom(String nom);
+
+    Boolean existsByEmail(String email);
+
+    Boolean existsByCin(String cin);
+
 }
